b9fbd04a9ddf80eba6854100f54c94b8db2d6a2d
otm-ane0 <hammadiotmane21@gmail.com>
2025-09-16 02:31:01 +0100

    Fix merge conflicts in Question.php model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -9,25 +9,17 @@ class Question extends Model
 {
     protected $fillable = [
         'question',
-<<<<<<< HEAD
         'images',
         'question_image_path',
         'answer_image_path',
-        'answer',
-        'latex_question',
-        'latex_answer',
-=======
         'image',
         'options',
-<<<<<<< HEAD
-        'answer',
-=======
         'options_images',
         'text_options',
         'answer',
         'answer_image',
->>>>>>> a0c595f5a6fd462401a4dc2125a6b45408cc7c90
->>>>>>> fdaa374b8c473690086850ea4e7af998f74c278c
+        'latex_question',
+        'latex_answer',
         'difficulty',
         'question_type',
         'education_level',
@@ -41,21 +33,12 @@ class Question extends Model
         'answer_doc',
     ];
 
-<<<<<<< HEAD
     protected $casts = [
         'images' => 'array',
-    ];
-
-=======
-<<<<<<< HEAD
-=======
-    protected $casts = [
         'options_images' => 'array',
         'text_options' => 'array',
     ];
 
->>>>>>> a0c595f5a6fd462401a4dc2125a6b45408cc7c90
->>>>>>> fdaa374b8c473690086850ea4e7af998f74c278c
     public function topic(){
         return $this->belongsTo(Topic::class);
     }
